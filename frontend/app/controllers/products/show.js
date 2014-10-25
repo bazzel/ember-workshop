366c469bda39c4ea41c537a4c0ae4e907229805a
Patrick Baselier <patrick@kabisa.nl>
2014-10-25 14:03:18 +0200

    Edit product

---
+++ frontend/app/controllers/products/show.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.ObjectController.extend({
+  isEditing: false,
+  actions: {
+    toggleEditing: function() {
+      this.toggleProperty('isEditing');
+      this.model.rollback();
+    },
+    submit: function() {
+      var _this = this;
+
+      this.model.save().then(function() {
+        _this.set('isEditing', false);
+      });
+    }
+  }
+});
--- frontend/app/routes/products/show.js
+++ frontend/app/routes/products/show.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    willTransition: function() {
+      var book = this.currentModel;
+
+      if (book.get('isDirty')) {
+        book.rollback();
+      }
+      this.controller.set('isEditing', false);
+    }
+  }
 });
