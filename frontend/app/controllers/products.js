19ed01da041e75f396d5f2e6dc0be5968abe6f57
Patrick Baselier <patrick@kabisa.nl>
2014-10-26 10:42:07 +0100

    Add search

--- frontend/app/controllers/products.js
+++ frontend/app/controllers/products.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+  needs: ['application'],
+  search: Ember.computed.alias('controllers.application.search'),
+  filteredContent: function() {
+    var products = this.model,
+        search = new RegExp(this.get('search'), 'i');
+
+    products = products.filter(function(product) {
+      var str = product.get('title') + product.get('description');
+      return search.test(str);
+    });
+    return products;
+  }.property('@each.title', '@each.description', 'search')
 });
