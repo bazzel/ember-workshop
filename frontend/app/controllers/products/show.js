860691a7af7d6035852a51ad3ede56b83e36e026
Patrick Baselier <patrick@kabisa.nl>
2014-10-28 14:57:21 +0100

    Add server side validations and show errors

--- frontend/app/controllers/products/show.js
+++ frontend/app/controllers/products/show.js
@@ -5,17 +5,38 @@ export default Ember.ObjectController.extend({
   actions: {
     toggleEditing: function() {
       this.toggleProperty('isEditing');
-      this.model.rollback();
+      this.resetModel();
     },
     submit: function() {
       var _this = this;
 
+      this.clearValidationErrors();
+
       this.model.save().then(function() {
         _this.set('isEditing', false);
+      }, function(reason) {
+        _this.set('validationErrors', reason.errors);
       });
     },
     removeImage: function() {
       this.set('image', null);
     }
+  },
+  clearValidationErrors: function() {
+    if (!this.model.get('isValid')) {
+      this.set('validationErrors', null);
+      this.model.rollback();
+    }
+  },
+  cleanModel: function() {
+    var model = this.model;
+
+    if (model.get('isDirty')) {
+      model.rollback();
+    }
+  },
+  resetModel: function() {
+    this.clearValidationErrors();
+    this.cleanModel();
   }
 });
--- frontend/app/routes/products/show.js
+++ frontend/app/routes/products/show.js
@@ -3,11 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   actions: {
     willTransition: function() {
-      var book = this.currentModel;
-
-      if (book.get('isDirty')) {
-        book.rollback();
-      }
+      this.controller.resetModel();
       this.controller.set('isEditing', false);
     }
   }
