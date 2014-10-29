aa1df01791b87cc8af9e8ce86516df699cde91ce
Patrick Baselier <patrick@kabisa.nl>
2014-10-29 09:15:13 +0100

    Add defaultValue to review

--- frontend/app/models/review.js
+++ frontend/app/models/review.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   description: DS.attr(),
-  rating: DS.attr(),
+  rating: DS.attr('number', { defaultValue: 0 }),
   user: DS.attr(),
   createdAt: DS.attr()
 });
